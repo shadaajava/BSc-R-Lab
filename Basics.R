130b4297a81234ac4633f2d6374ff1043cd62d26
Shadaan <shadaankabula@gmail.com>
2026-03-29 18:08:07 +0530

    Create Basics.R

---
+++ Basics.R
@@ -0,0 +1,10 @@
+# My first R Program for 4th Semester
+print("Hello R Programming!")
+
+# Creating a simple sequence of numbers
+numbers <- c(10, 20, 30, 40, 50)
+print(numbers)
+
+# Calculating the Mean (Average)
+result <- mean(numbers)
+print(paste("The average is:", result))
