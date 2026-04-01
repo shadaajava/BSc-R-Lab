e3c11aaf77c66aca01c5be647619569e379dd7c0
Shadaan <shadaankabula@gmail.com>
2026-04-01 18:24:48 +0530

    Create R-Vector-Arithmetic-All.R

---
+++ R-Vector-Arithmetic-All.R
@@ -0,0 +1,22 @@
+a <- c(2, 3.3, 4)
+b <- c(11, 5, 3)
+# 1.addition
+print(a+b)
+
+# 1. Subtraction
+print(a - b)
+
+# 2. Multiplication
+print(a * b)
+
+# 3. Division
+print(a / b)
+
+# 4. Modulo (Remainder)
+print(a %% b)
+
+# 5. Integer Division (Quotient)
+print(a %/% b)
+
+# 6. Exponentiation (Power)
+print(a ^ b)
